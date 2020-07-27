3d6422dfd37bab6d34a9d87413b600649bd6c56e
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-07-28 02:43:21 +0300

    fix

--- src/engine/openmp.cc
+++ src/engine/openmp.cc
@@ -39,7 +39,7 @@ OpenMP *OpenMP::Get() {
 }
 
 OpenMP::OpenMP()
-  : omp_num_threads_set_in_environment_(is_env_set("OMP_NUM_THREADS")) {
+  : omp_num_threads_set_in_environment_(true) {
 #ifdef _OPENMP
   initialize_process();
   const int max = dmlc::GetEnv("MXNET_OMP_MAX_THREADS", INT_MIN);
